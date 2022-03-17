aa61e99974ce5b6249df808238cfe63df6db6902
onggi <hsk1533@naver.com>
2022-03-17 14:14:01 +0900

    init

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<LoginScreen> createState() => _LoginScreen();
+}
+
+class _LoginScreen extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screens/mainScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  State<MainScreen> createState() => _MainScreen();
+}
+
+class _MainScreen extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
